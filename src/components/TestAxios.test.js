18142903256f3de4bc2da513af126186f73d4744
bhubhik <bhubhik@gmail.com>
2022-07-04 19:49:30 +1000

    AxiosMock call, and test

--- src/components/TestAxios.test.js
+++ src/components/TestAxios.test.js
@@ -12,3 +12,18 @@ it('Should be displaying Loading', () => {
   //OR
   // expect(getByTestId('loading')).toHaveTextContent('Loading...');
 });
+
+it('should display and load the data', async () => {
+  const url = '/greeting';
+  const { getByTestId } = render(<TestAxios url={url} />);
+  axiosMock.get.mockResolvedValueOnce({
+    data: {
+      greeting: 'Hello there',
+    },
+  });
+  fireEvent.click(getByTestId('fetch-data'));
+  const greetingData = await waitForElement(() => getByTestId('show-data'));
+  expect(axiosMock.get).toHaveBeenCalledTimes(1);
+  expect(axiosMock.get).toBeCalledWith(url);
+  expect(greetingData).toHaveTextContent('Hello there');
+});
