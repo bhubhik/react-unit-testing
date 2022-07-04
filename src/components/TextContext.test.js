95eb7119c89e4c284c40d74c22e4d739d6232b32
bhubhik <bhubhik@gmail.com>
2022-07-04 14:05:21 +1000

    Rendering with context

--- src/components/TextContext.test.js
+++ src/components/TextContext.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { cleanup, render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import CounterProvider, { CounterContext, counter } from './TestContext';
+import CounterProvider, { CounterContext, Counter } from './TestContext';
 
 const renderWithContext = (component) => {
   return {
@@ -14,6 +14,18 @@ const renderWithContext = (component) => {
 afterEach(cleanup);
 
 it('check if the initial value is 0', () => {
-  const { getByTestId } = renderWithContext(<counter />);
+  const { getByTestId } = renderWithContext(<Counter />);
   expect(getByTestId('counter')).toHaveTextContent('0');
+});
+
+it('checks if it increments', () => {
+  const { getByTestId } = renderWithContext(<Counter />);
+  fireEvent.click(getByTestId('button-up'));
+  expect(getByTestId('counter')).toHaveTextContent('1');
+});
+
+it('checks if the number decrements', () => {
+  const { getByTestId } = renderWithContext(<Counter />);
+  fireEvent.click(getByTestId('button-down'));
+  expect(getByTestId('counter')).toHaveTextContent('-1');
 });
